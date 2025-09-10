0dde4a203aa7c068d907f8011e87f66996cf32e0
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2025-09-10 12:30:48 +0200

    (fix): use new forge eventbus system

--- forge/src/main/java/com/tristankechlo/random_mob_sizes/ForgeRandomMobSizes.java
+++ forge/src/main/java/com/tristankechlo/random_mob_sizes/ForgeRandomMobSizes.java
@@ -3,7 +3,6 @@ package com.tristankechlo.random_mob_sizes;
 import com.tristankechlo.random_mob_sizes.commands.MobScalingsCommand;
 import com.tristankechlo.random_mob_sizes.commands.RandomMobSizesCommand;
 import com.tristankechlo.random_mob_sizes.config.ConfigManager;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -12,9 +11,8 @@ import net.minecraftforge.fml.common.Mod;
 public class ForgeRandomMobSizes {
 
     public ForgeRandomMobSizes() {
-        MinecraftForge.EVENT_BUS.addListener(this::onServerStart);
-        MinecraftForge.EVENT_BUS.addListener(this::registerCommands);
-        MinecraftForge.EVENT_BUS.register(this);
+        ServerStartingEvent.BUS.addListener(this::onServerStart);
+        RegisterCommandsEvent.BUS.addListener(this::registerCommands);
     }
 
     private void onServerStart(final ServerStartingEvent event) {
